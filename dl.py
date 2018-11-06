8f5b25a2c21fc2afaa0ee7cd83e9c2d70e8425d2
zychp <pawelzych97@gmail.com>
2018-11-07 00:03:43 +0100

    login app divided into webapp and dl,  sesion dict on redis, html links fix

---
+++ dl.py
@@ -0,0 +1,68 @@
+from flask import Flask, request, redirect, send_from_directory
+from werkzeug.utils import secure_filename
+import json
+import os
+import uuid
+import jwt
+
+app = Flask(__name__)
+
+app.config.from_pyfile('NoSecretThere.cfg')  # for SECRET_KEY
+secret_jwt= 'testSecret'
+
+username = "Dummy"
+
+@app.route('/zychp/dl/getfileslist', methods=['POST'])
+def getFilesList(username):
+    if (auth()):
+        return listUserFiles(username)
+    else:
+        return "No auth"
+
+
+@app.route('/zychp/dl/download/<path:filename>', methods=['POST'])
+def download(filename):
+    if (auth()):
+         userpath = getUserDirPath(username)
+         return send_from_directory(directory=userpath, filename=filename)
+    else:
+        return "No auth"
+   
+
+@app.route('/zychp/dl/upload', methods=['POST'])
+def upload():
+    if (auth()):
+        n_to_upload = 5-countUserFiles(username)
+        userpath = getUserDirPath(username)
+        n_uploaded = 0
+        for filee in request.files:
+            if(n_uploaded < n_to_upload):
+                f = request.files[filee]
+                f.save(userpath + secure_filename(f.filename))
+                n_uploaded += 1
+        return "Files uploaded"
+    else:
+        return "No auth"
+     
+
+def listUserFiles(username):
+    userpath = getUserDirPath(username)
+    listed_files = os.listdir(userpath)
+    for i in range(len(listed_files), 5):
+        listed_files.append("Brak pliku")
+    return listed_files
+
+def countUserFiles(username):
+    return len(os.listdir(getUserDirPath(username)))
+
+def crateUploadDirectoryIfNotExist(username):
+    userpath = getUserDirPath(username)
+    if not os.path.exists(userpath):
+        os.makedirs(userpath)
+    return
+
+def getUserDirPath(username):
+    return 'userfiles/' + username + '/'
+
+def auth():
+    return True
---
+++ webapp.py
@@ -0,0 +1,129 @@
+from flask import Flask, session, request, redirect, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import json, uuid, redis, jwt
+
+app = Flask(__name__)
+
+app.config.from_pyfile('NoSecretThere.cfg')  # for SECRET_KEY
+app.config['SESSION_COOKIE_SECURE'] = True
+app.config['SESSION_COOKIE_PATH'] = "/zychp/webapp"
+
+secret_jwt = 'testSecret'
+
+red = redis.Redis()
+
+@app.route('/zychp/webapp/base') 
+def baseTest():
+    return render_template("base.html")
+
+@app.route('/zychp/webapp/register')
+def registerTest():
+    return render_template("register.html")
+
+
+@app.route('/zychp/webapp/login', methods=['GET', 'POST'])
+def login():
+    if (checkUserLogin()):
+        return render_template("base.html", message="Już zalogowany.")
+    else:
+        if request.method == 'POST':
+            if doLogin():
+                return redirect("/zychp/webapp/fileslist")
+            else:
+                return render_template("login.html", error="Niepoprawny login lub/i hasło")
+        else:
+            return render_template("login.html")
+
+
+@app.route('/zychp/webapp/logout')
+def logout():
+    username = checkUserLogin()
+    if (username):
+        cookie_uuid = session['uuid']
+        red.hdel('zychp:webapp:'+ cookie_uuid, 'login')
+
+        #session.pop('username',None)
+        #session.pop('uuid',None)
+    return redirect('/zychp/webapp/login')
+
+
+@app.route('/zychp/webapp/fileslist', methods=['GET', 'POST'])
+def filesList():
+    username = checkUserLogin()
+    if (username):
+        print("Pobierz liste")
+        if request.method == 'POST':
+            print("POST fileslist")
+        else:
+            return render_template("fileslist.html", username=username)
+            #return render_template("fileslist.html", username=username, file1=listed_files[0],
+            #                   file2=listed_files[1],  file3=listed_files[2], file4=listed_files[3], file5=listed_files[4])
+    else:                  
+        return render_template("base.html", message='Nie zalogowano.')
+
+
+@app.route('/zychp/webapp/upload', methods=['GET', 'POST'])
+def upload():
+    username = checkUserLogin()
+    n_to_upload = 5
+    if (username):
+        if request.method == 'POST':
+            return redirect('/zychp/webapp/fileslist')
+        else:
+            return render_template("upload.html", username=username, n_to_upload=n_to_upload)
+    return render_template("base.html", message='Nie zalogowano.')
+
+
+def doLogin():
+    username = request.form['username']
+    users_credentials = getUsersCredentials()
+
+    for user in users_credentials:
+        if username == user and request.form['password'] == users_credentials[user]:
+            user_uuid = str(uuid.uuid4())
+            session['username'] = username
+            session['uuid'] = user_uuid
+
+            red.hset('zychp:webapp:'+ user_uuid, 'login', username)
+
+            #crateUploadDirectoryIfNotExist(username)
+            return True
+    return False
+
+
+def checkUserLogin():
+    users_credentials = getUsersCredentials()
+    if(session):
+        cookie_username = session['username']
+        cookie_uuid = session['uuid']
+        redis_username = red.hget('zychp:webapp:'+ cookie_uuid, 'login')
+        if redis_username is None:
+            redis_username="NONE"
+        else:
+            redis_username=redis_username.decode('utf-8')
+
+        print("Uuid: {}".format(cookie_uuid))
+        print("Cookie_username: {}".format(cookie_username))
+        print("Redis_username: {}".format(redis_username))
+
+        if cookie_username in users_credentials:
+            if(redis_username == cookie_username):
+                return cookie_username
+            else:
+                print("Brak wpisu w redis, podrzucone ciastko")
+                return False
+        else:
+            print("Brak loginu w bazie")
+            return False
+    else:
+        print("Brak ciastka")
+        return False
+
+
+def getUsersCredentials():
+    dbfile = open('database.json', 'r')
+    database = json.loads(dbfile.read())
+    users_credentials = {}
+    for user_data in database['userslist']:
+        users_credentials[user_data['login']] =  user_data['password']      
+    return users_credentials
