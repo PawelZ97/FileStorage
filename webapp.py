323865108152b470bc2dcc011c6052ae6627fb2c
PAwelZ97 <pawelzych97@gmail.com>
2018-12-12 15:54:43 +0100

    auth0 login

--- webapp.py
+++ webapp.py
@@ -1,6 +1,13 @@
-from flask import Flask, session, request, redirect, render_template, send_from_directory
+from flask import Flask, session, request, redirect, render_template, send_from_directory, jsonify, url_for
 from werkzeug.utils import secure_filename
 import json, uuid, redis, jwt, os, datetime, hashlib
+
+from functools import wraps
+from os import environ as env
+from werkzeug.exceptions import HTTPException
+#from dotenv import load_dotenv, find_dotenv
+from authlib.flask.client import OAuth
+from six.moves.urllib.parse import urlencode
 
 
 app = Flask(__name__)
@@ -10,8 +17,33 @@ app.config['SESSION_COOKIE_SECURE'] = True
 app.config['SESSION_COOKIE_PATH'] = "/zychp/webapp"
 
 secret_jwt = open('NoSecretThere.cfg', 'rb').read().decode('utf-8')
+secret_auth0 = open('NoSecondSecretThere.cfg', 'rb').read().decode('utf-8')
 
 red = redis.Redis()
+
+oauth = OAuth(app)
+
+auth0 = oauth.register(
+    'auth0',
+    client_id='sPkHamObJWj65rG__BOkn9g9hLhdJ5Bc',
+    client_secret=secret_auth0,
+    api_base_url='https://zychp.eu.auth0.com',
+    access_token_url='https://zychp.eu.auth0.com/oauth/token',
+    authorize_url='https://zychp.eu.auth0.com/authorize',
+    client_kwargs={
+        'scope': 'openid profile',
+    },
+)
+
+def requires_auth(f):
+  @wraps(f)
+  def decorated(*args, **kwargs):
+    if 'profile' not in session:
+      # Redirect to Login page here
+      return redirect('/zychp/webapp/login')
+    return f(*args, **kwargs)
+
+  return decorated
 
 @app.route('/zychp/webapp/base') 
 def baseTest():
@@ -21,38 +53,42 @@ def baseTest():
 @app.route('/zychp/webapp/register')
 def registerTest():
     return render_template("register.html")
+  
 
-
-@app.route('/zychp/webapp/login', methods=['GET', 'POST'])
+@app.route('/zychp/webapp/login', methods=['GET','POST'])
 def login():
-    if (checkUserLogin()):
-        return render_template("base.html", message="Już zalogowany.")
+    if request.method == 'POST':
+        return auth0.authorize_redirect(redirect_uri='https://edi.iem.pw.edu.pl/zychp/webapp/callback', audience='https://zychp.eu.auth0.com/userinfo')
     else:
-        if request.method == 'POST':
-            if doLogin():
-                return redirect("/zychp/webapp/fileslist")
-            else:
-                return render_template("login.html", error="Niepoprawny login lub/i hasło")
-        else:
-            return render_template("login.html")
-
+        return render_template("login.html")
 
 @app.route('/zychp/webapp/logout')
 def logout():
-    username = checkUserLogin()
-    if (username):
-        cookie_uuid = session['uuid']
-        red.hdel('zychp:webapp:'+ cookie_uuid, 'login')
+    # Clear session stored data
+    session.clear()
+    # Redirect user to logout endpoint
+    params = {'returnTo': url_for('login', _external=True), 'client_id': 'sPkHamObJWj65rG__BOkn9g9hLhdJ5Bc'}
+    return redirect(auth0.api_base_url + '/v2/logout?' + urlencode(params))
 
-        red.hdel('zychp:webapp:userfiles'+ getHash(username), 'fileslist')
-        red.hdel('zychp:webapp:userfiles'+ getHash(username), 'nfiles')
+@app.route('/zychp/webapp/callback')
+def callback_handling():
+    # Handles response from token endpoint
+    auth0.authorize_access_token()
+    resp = auth0.get('userinfo')
+    userinfo = resp.json()
 
-        session.pop('username',None)
-        session.pop('uuid',None)
-    return redirect('/zychp/webapp/login')
+    # Store the user information in flask session.
+    session['jwt_payload'] = userinfo
+    session['profile'] = {
+        'user_id': userinfo['sub'],
+        'name': userinfo['name'],
+        'picture': userinfo['picture']
+    }
+    return redirect('/zychp/webapp/fileslist')
 
 
 @app.route('/zychp/webapp/fileslist')
+@requires_auth
 def filesList():
     username = checkUserLogin()
     if (username):
@@ -67,20 +103,21 @@ def filesList():
         return redirect('/zychp/webapp/login')
     
 @app.route('/zychp/webapp/sharelink', methods=['POST'])
+@requires_auth
 def share():
     username = checkUserLogin()
     if (username):
         filename = request.form['filename']
         if(filename):
             jwt_value = jwt.encode({'username': username,'filename': filename},secret_jwt, algorithm='HS256').decode('utf-8')
-            link = "https://pi.iem.pw.edu.pl/zychp/dl/sharedl/" + jwt_value
+            link = "https://edi.iem.pw.edu.pl/zychp/dl/sharedl/" + jwt_value
             return render_template("share.html", link = link)
         else:
             return render_template("base.html", message='Wystąpił błąd.')
     return render_template("base.html", message='Nie zalogowano.')
 
-
 @app.route('/zychp/webapp/upload', methods=['GET', 'POST'])
+@requires_auth
 def upload():
     username = checkUserLogin()
     if (username):
@@ -94,61 +131,17 @@ def upload():
         return render_template("upload.html", username=username, n_to_upload=n_to_upload, iterlist=range(1,n_to_upload+1), jwt_value=jwt_value)
     return render_template("base.html", message='Nie zalogowano.')
 
-
-def doLogin():
-    username = request.form['username']
-    users_credentials = getUsersCredentials()
-
-    for user in users_credentials:
-        hashpass = hashlib.sha256(request.form['password'].encode()).hexdigest() 
-        if username == user and hashpass == users_credentials[user]:
-            user_uuid = str(uuid.uuid4())
-            session['username'] = username
-            session['uuid'] = user_uuid
-
-            red.hset('zychp:webapp:'+ user_uuid, 'login', username)
-            
-            red.hdel('zychp:webapp:userfiles'+ getHash(username), 'fileslist')
-            red.hdel('zychp:webapp:userfiles'+ getHash(username), 'nfiles')
-            return True
-    return False
+@app.route('/zychp/webapp/dashboard')
+@requires_auth
+def dashboard():
+    return render_template('dashboard.html',
+                           userinfo=session['profile'],
+                           userinfo_pretty=json.dumps(session['jwt_payload'], indent=4))
 
 def checkUserLogin():
-    users_credentials = getUsersCredentials()
-    if(session):
-        cookie_username = session['username']
-        cookie_uuid = session['uuid']
-        redis_username = red.hget('zychp:webapp:'+ cookie_uuid, 'login')
-        if redis_username is None:
-            redis_username="NONE"
-        else:
-            redis_username=redis_username.decode('utf-8')
-
-        #print("Uuid: {}".format(cookie_uuid))
-        #print("Cookie_username: {}".format(cookie_username))
-        #print("Redis_username: {}".format(redis_username))
-
-        if cookie_username in users_credentials:
-            if(redis_username == cookie_username):
-                return cookie_username
-            else:
-                print("Brak wpisu w redis, podrzucone ciastko")
-                return False
-        else:
-            print("Brak loginu w bazie")
-            return False
-    else:
-        print("Brak ciastka")
-        return False
-
-def getUsersCredentials():
-    dbfile = open('database.json', 'r')
-    database = json.loads(dbfile.read())
-    users_credentials = {}
-    for user_data in database['userslist']:
-        users_credentials[user_data['login']] =  user_data['password']      
-    return users_credentials
-
+    nickname = session['profile']['name']
+    return nickname
+   
 def getHash(string):
     string += secret_jwt
     return hashlib.sha256(string.encode()).hexdigest() 
